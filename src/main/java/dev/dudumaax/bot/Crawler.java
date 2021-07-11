4e48be8d036cdf9fcfcff1b197c3856e52fb8db1
dmaax <dudzcool@gmail.com>
2021-07-11 02:57:56 -0300

    Fixed crawler structure

--- src/main/java/dev/dudumaax/bot/Crawler.java
+++ src/main/java/dev/dudumaax/bot/Crawler.java
@@ -8,20 +8,21 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Crawler {
-		
+
 	public String returnImage(String url) {
-		if(!url.startsWith("https://prnt.sc/")) return url;
+		if (!url.startsWith("https://prnt.sc/"))
+			return url;
 		try {
 			Document doc = Jsoup.connect(url).get();
 			Elements divs = doc.getElementsByTag("div");
 			for (Element el : divs) {
-				if(el.hasClass("image-constrain js-image-wrap")) {
+				if (el.hasClass("image-constrain js-image-wrap")) {
 					Elements div2 = el.getAllElements();
 					for (Element divfinalmaybe : div2) {
-						if(divfinalmaybe.hasClass("image-container image__pic js-image-pic")) {
+						if (divfinalmaybe.hasClass("image-container image__pic js-image-pic")) {
 							Elements imgs = divfinalmaybe.getAllElements();
-							for(Element img : imgs) {
-								if(img.hasClass("no-click screenshot-image")) {
+							for (Element img : imgs) {
+								if (img.hasClass("no-click screenshot-image")) {
 									String src = img.absUrl("src");
 									return src;
 								}
